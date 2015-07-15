fb9da1fd1844ef3818d9c7c68c89689ddc1972a4
Daniel Shumway <shumway.danny@gmail.com>
2015-07-14 22:40:24 -0400

    Researching path woes.

--- source/build/deploy.sh
+++ source/build/deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e # exit with nonzero exit code if anything fails
 
+#jump back to correct directory
+cd ../../
+
 # clear and re-create the out directory
 rm -rf out || exit 0;
 mkdir out;
