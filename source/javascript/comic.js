e45c89c7858407b991a98a484d1080d85b8b4d2d
Daniel Shumway <shumway.danny@gmail.com>
2015-07-18 22:31:30 -0400

    Some small javascripts

---
+++ source/javascript/comic.js
@@ -0,0 +1,56 @@
+(function() {
+    'use strict';
+
+    var key = 'crawlspace-mode'; //for localstorage.
+    window.comic = {
+        mode : 'off', //default
+        filters : ['off', 'on'], //total filters we can have.
+        elements : {}, //autofilled at runtime
+
+        //adds elements and variables.
+        init : function() {
+            var that=this, i;
+            if(typeof(Storage) === "undefined") {
+                return; //Sorry ie7.
+            }
+
+            for(i=0;i<that.filters.length;i++) {
+                elements[that.filters[i]] = document.getElementById('comic-' + that.filters[i]);
+            }
+
+            document.getElementById('comic-base')
+            that.loadLocalStorage();
+        },
+        //Refreshes images based on filters selected.
+        updateComic : function() {
+            var that=this, i;
+
+            for(i=0;i<that.filters.length;i++) {
+                if(that.filters[i] === that.mode) {
+                    that.elements[that.filters[i]].style.display = 'block';
+                } else {
+                    that.elements[that.filters[i]].style.display = 'block';
+                }
+            }
+        },
+        //Reads from localStorage, and corrects any errors that might exist.
+        loadLocalStorage() {
+            var mode = localStorage.getItem(that.key);
+
+            if (that.filters.indexOf(mode) === -1) {
+                localStorage.setItem(that.key, that.mode);
+            } else {
+                that.mode = mode;
+            }
+
+            that.updateComic();
+        },
+        //saves to local
+        saveLocalStorage(mode, toggle) {
+            that.mode = mode;
+            localStorage.setItem(that.key, mode);
+            //toggle.class = //Swap buttons?
+            that.updateComic();
+        },
+    };
+}());
